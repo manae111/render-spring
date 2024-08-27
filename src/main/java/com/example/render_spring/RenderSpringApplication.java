3949db454c24a622826c3d3e9bd8fe63f06bdd39
Sakai <manae.sakai@rakus-partners.co.jp>
2024-08-27 17:53:29 +0900

    最初のコミット

---
+++ src/main/java/com/example/render_spring/RenderSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.render_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenderSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenderSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/render_spring/RenderSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.render_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
